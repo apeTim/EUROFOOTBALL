cb57b08818edc463e584f9a0089d43118e72b7a8
l4kk3r <66743739+l4kk3r@users.noreply.github.com>
2021-04-26 20:21:49 +0300

    added rating to bot

--- bot.py
+++ bot.py
@@ -2,22 +2,24 @@ from telegram import ReplyKeyboardMarkup, Bot
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler, ConversationHandler
 from classes.BotMainFunctionsFile import BotMainFunctions
+from classes.UserFunctionsFile import UserFunctions
 from classes.ConversationScenariosFile import ConversationScenarios
 
 def main():
-    updater = Updater('1708143492:AAFwkcrw6VYrem2d_q4hta4-HgfTGT45XHI', use_context=True)
-    bot = Bot('1708143492:AAFwkcrw6VYrem2d_q4hta4-HgfTGT45XHI')
+    updater = Updater('1503161381:AAFMuTwXBUltIBvUYTtLmUWVEB0-1IS8UBk', use_context=True)
+    bot = Bot('1503161381:AAFMuTwXBUltIBvUYTtLmUWVEB0-1IS8UBk')
     dp = updater.dispatcher
 
     conversation_scenario = ConversationScenarios(bot)
     bot_functions = BotMainFunctions()
-
+    user_functions = UserFunctions(bot)
 
     dp.add_handler(CommandHandler("start", bot_functions.start_command))
     dp.add_handler(conversation_scenario.user_listings_scenario())
     dp.add_handler(conversation_scenario.seller_conversation_scenario())
     dp.add_handler(conversation_scenario.buyer_conversation_scenario())
-    dp.add_handler(MessageHandler(Filters.text, bot_functions.any_text))
+    dp.add_handler(conversation_scenario.rate_user_scenario())
+    dp.add_handler(MessageHandler(Filters.regex("Мой профиль"), user_functions.user_profile))
 
     updater.start_polling()
     updater.idle()
